96213679bf9befbca824d9ae205ce08355d1ccd8
Curro Portillo <211curroportillo@gmail.com>
2025-10-31 18:16:54 +0100

    ejercicio 27

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero = Integer.parseInt(System.console().readLine("Introduzca un numero mayor que 1: "));
+        int suma = 0;
+        int contador = 0;
+        
+        for(int i= 1; i<=numero; i++){     
+            if(i % 3 == 0){
+                System.out.print(i + " ");
+                suma += i;
+                contador++;
+            }        
+        }
+        System.out.println();
+        System.out.printf("Desde 1 hasta %d hay %d múltiplos de 3 y suman %d", numero, contador, suma);
     }
 }
